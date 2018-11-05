395e95885080a20b4b152c9faa30d344a7adeadb
Chelak Stas <yaddaa2@gmail.com>
2018-11-05 05:37:00 +0200

    fix wrong target in storyboard, fix constraint issue

--- CSSimpleSideMenu.podspec
+++ CSSimpleSideMenu.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'CSSimpleSideMenu'
-  s.version          = '1.0.4'
+  s.version          = '1.0.5'
   s.summary          = 'A simple iOS side menu for small projects.'
 
 # This description is used to generate tags and improve search results.
--- CSSimpleSideMenu/Extensions/UIViewController+Extensions.swift
+++ CSSimpleSideMenu/Extensions/UIViewController+Extensions.swift
@@ -2,12 +2,12 @@ import UIKit
 
 extension UIViewController {
     
-    class func instance() -> Self {
-        return instantiateFromStoryboardHelper(String(describing: self))
+    static func instance() -> Self {
+        return instantiateFromStoryboardHelper()
     }
     
-    fileprivate class func instantiateFromStoryboardHelper<T>(_ name: String) -> T {
-        let storyboard = UIStoryboard(name: name, bundle: Bundle(for: self))
+    fileprivate class func instantiateFromStoryboardHelper<T>() -> T {
+        let storyboard = UIStoryboard(name: String(describing: self), bundle: Bundle(for: self))
         let identifier = String(describing: self)
         let controller = storyboard.instantiateViewController(withIdentifier: identifier) as! T
         return controller
